710bfdc0a128967800a06f5325381f129df0ad08
么 S U B → Z E R O 么 <142734862+GangZero@users.noreply.github.com>
2025-01-08 23:06:49 -0600

    Delete GameModules directory

--- GameModules/Anti Cheat Bypass.lua
+++
@@ -1,7 +0,0 @@
-for _, obj in ipairs(game:GetDescendants()) do
-    if obj:IsA("Script") or obj:IsA("LocalScript") or obj:IsA("RemoteEvent") or obj:IsA("RemoteFunction") then
-        if obj.Source and obj.Source:match("\n") then
-            obj:Destroy()
-        end
-    end
-end
